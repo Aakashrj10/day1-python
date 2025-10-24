75b0fca008bb76a11abdace102561a8b81c1a26c
Aakash <aakashrj777@gmail.com>
2025-10-24 12:40:30 +0530

    Day1 -First Programs

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+print("MY name is AAkash rj")
+
+print(10 + 20+5*2)
+
+name = "Aakash"
+age=20
+print("My name is", name, "and I am" , age," years old.")
+
+Age = int(input("Enter your age: "))
+
+if Age >=18:
+    print("You are an adult!")
+else:
+    print("You are a minor!")
