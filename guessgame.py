c9e3ab6a9281a3cc79d35a02f97b45fc101bdc9c
Aakash <aakashrj777@gmail.com>
2025-10-28 22:55:02 +0530

    Begineer Projects

---
+++ guessgame.py
@@ -0,0 +1,13 @@
+import random
+
+number= random.randint(1,100)
+guess=0
+
+while guess!= number:
+    guess=int(input("Enter your guess between 1 and 100: "))
+    if guess < number:
+        print("Too Low ! Try again.")
+    elif guess > number :
+        print("Too High ! Try again.")
+    elif guess == number:
+        print("You got it right! The number was", number)
